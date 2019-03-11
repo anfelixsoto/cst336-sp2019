353958a9b23178ae87c1fd42037cf7cbeec4b65d
Antonio Felix <anfelix@csumb.edu>
2019-03-11 23:59:24 +0000

    added css predence testing

--- lab/lab5/randomPassword.php
+++ lab/lab5/randomPassword.php
@@ -7,6 +7,8 @@ return substr(str_shuffle($chars),0,$length);
 
 }
 
-echo rand_string(20);
+$password = array();
+$password["generatedPassword"] = rand_string(20);
+echo json_encode($password);
 
 ?>
