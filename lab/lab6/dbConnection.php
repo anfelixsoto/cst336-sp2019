398aa5cd52354370dde8e7da8dd2288846850669
Antonio Felix <anfelix@csumb.edu>
2019-04-17 01:15:43 +0000

    hell

--- lab/lab6/dbConnection.php
+++ lab/lab6/dbConnection.php
@@ -1,25 +1,24 @@
 <?php
 
-function getDatabaseConnection($dbname = "ottermart"){
+function getDatabaseConnection(){
+    $connUrl = getenv('JAWSDB_MARIA_URL');
+    $hasConnUrl = !empty($connUrl);
+    $connParts = null;
+    if ($hasConnUrl) {
+        $connParts = parse_url($connUrl);
+    }
     
-    //C9 db info
-    $host = "localhost";
-    $username = ”root”;
-    $password = "";
-    
-    //when connecting from Heroku
-    if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
-        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-        $host = $url["host"];
-        $dbname = substr($url["path"], 1);
-        $username = $url["user"];
-        $password = $url["pass"];
-    } 
+    $host = $hasConnUrl ? $connParts['host']: getenv('IP');
+    $dbname = $hasConnUrl ? ltrim($connParts['path'],'/') : 'ottermart';
+    $username = $hasConnUrl ? $connParts['user'] : getenv('antoniofelix118');
+    $password = $hasConnUrl ? $connParts['pass'] : '';
     
     $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-    $dbConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+    
+    $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
     return $dbConn;
+ 
 }
-
 ?>
+
