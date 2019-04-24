40cd59a909f2ded208ed423d6bd2805adf05f5b0
Antonio Felix <anfelix@csumb.edu>
2019-04-24 21:55:38 +0000

    monolog testing

--- lab/lab7/index.php
+++ lab/lab7/index.php
@@ -4,6 +4,12 @@
     if(!isset($_SESSION['email'])){
         header("Location:login.html");
     }
+    
+    require __DIR__ . '/../../vendor/autoload.php';
+
+    $log = new Monolog\Logger('monolog-test');
+    $log->pushHandler(new Monolog\Handler\StreamHandler(__DIR__ . '/../../app.log', Monolog\Logger::INFO));
+    $log->info('I am inside the monolog test page');
 ?>
 
 <!DOCTYPE html>
